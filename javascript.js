bff00e03259f4468a805c3393b2fd51c3f659eab
Ravindu Fernando <84976228+punk-rocher@users.noreply.github.com>
2022-03-13 00:13:24 +0530

    Hacker Rank

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
-
+return function(num){
+        console.log(weekdays,num);
+        if (Number(num) == -1 weekdays.length) {
+            throw new Error("Invalid weekday number")
+        }
+        return weekdays[Number(num)]
+    }
+}
